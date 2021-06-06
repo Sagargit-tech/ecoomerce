f1cc6e70e9cd4949c30bc640865f4de02d7b55aa
Sagargit-tech <78584787+Sagargit-tech@users.noreply.github.com>
2021-06-06 12:03:35 +0530

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from blog.models import Blog, Contact, Signup, Photo,comment
+# Register your models here.
+
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display=['sno','title']
+    class Media:
+        css={'all':("css/main.css",)}
+        js=("js/blog.js",)
+
+admin.site.register(Blog,BlogAdmin)
+
+    
+admin.site.register(Photo)
+admin.site.register(Contact)
+admin.site.register(Signup)
+admin.site.register(comment)
